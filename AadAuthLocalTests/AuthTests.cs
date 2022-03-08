d2e5d741b52c7bb43d154e0d034667c6e228e1f0
Scott Falbo <scottfalboart@gmail.com>
2022-03-07 22:56:40 -0800

    enhanced the test to assert on cert and token an output to test explorer

--- AadAuthLocalTests/AuthTests.cs
+++ AadAuthLocalTests/AuthTests.cs
@@ -1,11 +1,18 @@
 using System;
 using System.Threading.Tasks;
 using Xunit;
+using Xunit.Abstractions;
 
 namespace AadAuthLocalTests
 {
     public class AuthTests
     {
+        private readonly ITestOutputHelper _output;
+        public AuthTests(ITestOutputHelper output)
+        {
+            _output = output;
+        }
+
         [Fact]
         public async Task CanAuthenticate()
         {
@@ -13,8 +20,19 @@ namespace AadAuthLocalTests
             await builder.GetCertificateAsync("");
             await builder.GetAccessTokenAsync();
             var result = await builder.AuthorizeClient();
-            string expected = "OK";
-            Assert.Equal(result.ReasonPhrase, expected);
+
+            string expectedIssuer = "";
+            string expectedSubject = "";
+            string expectedResponse = "OK";
+
+            _output.WriteLine($"{builder._certificate}");
+            _output.WriteLine($"Token: {builder._accessToken}");
+            _output.WriteLine($"Response: {result}");
+
+            Assert.Equal(expectedIssuer, builder._certificate.Issuer);
+            Assert.Equal(expectedSubject, builder._certificate.Subject);
+            Assert.NotNull(builder._accessToken);
+            Assert.Equal(expectedResponse, result.ReasonPhrase);
         }
     }
 }
