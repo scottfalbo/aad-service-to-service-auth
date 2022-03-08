2c65e628c2cc63c857ebe62c90f334310cab52bd
Scott Falbo <scottfalboart@gmail.com>
2022-03-07 18:03:57 -0800

    added authentication audience

--- AadAuth/Startup.cs
+++ AadAuth/Startup.cs
@@ -7,6 +7,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi.Models;
 using System;
 using System.Collections.Generic;
@@ -27,7 +28,15 @@ namespace AadAuth
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                .AddJwtBearer(JwtBearerDefaults.AuthenticationScheme, options =>
+                {
+                    options.Authority = $"{Configuration["AzureAd:Instance"]}{Configuration["AzureAd:TenantId"]}";
+                    options.TokenValidationParameters = new TokenValidationParameters()
+                    {
+                        ValidAudience = Configuration["AzureAd:ClientId"]
+                    };
+                });
 
             services.AddControllers();
             services.AddSwaggerGen(c =>
