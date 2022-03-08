fb346e15223a81a3852ad58d9501531f3f445ab3
Scott Falbo <scottfalboart@gmail.com>
2022-03-07 17:57:42 -0800

    Add project files.

---
+++ AadAuth/Controllers/MagicalWard.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AadAuth.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class MagicalWard : ControllerBase
+    {
+        [HttpGet]
+        public string Get()
+        {
+            return "You disabled the magical wards and may enter!";
+        }
+    }
+}
---
+++ AadAuthLocalTests/AuthTests.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace AadAuthLocalTests
+{
+    public class AuthTests
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
